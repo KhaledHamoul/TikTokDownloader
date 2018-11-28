46aef137e1f84ff8ee67a1b176a74118d70e132b
khaledHamoul <fk_hamoul@esi.dz>
2018-11-28 17:53:05 +0100

    Added basic dom with basic styling and bootstrap

---
+++ resources/views/main.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>TicToc Converter</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+
+</head>
+<body>
+    <!-- Logo -->
+    <section class="container">
+        <div class="logo text-center">
+            <img class="d-block mx-auto" src="images/logo.png" alt="">
+        </div>
+    </section>
+    <!-- Search Bar-->
+    <section class="container search-bar-global-container text-center">
+        <div class="title-container">
+            <h1>Online TikTok Video Downloader</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras purus nisi, euismod id tincidunt eu, feugiat id elit. Nunc imperdiet placerat fermentum. Maecenas nec egestas nisi. Ut ex diam, finibus et mi at, venenatis pulvinar sem.</p>
+        </div>
+        <div class="search-bar-container container text-center">
+                <div class="row search-bar-row">
+                    <div class="col-sm-9 columns">
+                        <input type="search" placeholder="Enter Video Link" class="search-bar">
+                    </div>
+                    <div class="col-sm-3 columns">
+                        <button class="search-bar-submit">Download</button>
+                    </div>
+                </div>
+        </div>
+    </section>
+    <!-- Results -->
+    <section class="container">
+
+    </section>
+    <footer class="footer">
+        <div class="container text-center">
+            <span class="text-muted">TIKTOK</span>
+        </div>
+    </footer>
+    <!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
