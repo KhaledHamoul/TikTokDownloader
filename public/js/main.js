2fbbc9cf764eda66975957b563ee8a52c753ae2f
khaledHamoul <fk_hamoul@esi.dz>
2018-11-29 22:24:49 +0100

    Fixed design for multiple screen sizes

--- public/js/main.js
+++ public/js/main.js
@@ -2,37 +2,67 @@ $(document).ready(function(){
 
 
     var $loading = $('#main-loader').hide();
+    var $result_container = $('#result-container').hide();
+    var $url_text_container = $('.url-text-container');
+    var $url_text_response = $('#url-text-response');
     $(document)
     .ajaxStart(function () {
         $loading.show();
+        $result_container.hide();
+        $url_text_container.hide();
     })
     .ajaxStop(function () {
         $loading.hide();
     });
 
     $('#ajaxSubmit').click(function(e){
-      $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+      if($('#search-bar').val().startsWith('http://vm.tiktok.com/')){
+        if($('#search-bar').val() === 'http://vm.tiktok.com/'){
+            $url_text_response.text('Please specify the video link');
+            $url_text_container.show().delay(5000).fadeOut();
         }
-      });
-      $.ajax({
-          url: '/getvideo',
-          type : 'POST',
-          data: {
-              page: $('#search-bar').val()
-          },
-          dataType: 'JSON',
-          success: function(result){
-              console.log(result);
-              $('.result-container').show();
-              $('#videocover').attr('src',result.cover);
-              $('#videolink').attr('href',result.video);
-          },
-          error: function(jqXHR, textStatus, error){
-              console.log(error);
-          }
-      });
+        else{
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+              });
+              $.ajax({
+                  url: '/getvideo',
+                  type : 'POST',
+                  data: {
+                      page: $('#search-bar').val()
+                  },
+                  dataType: 'JSON',
+                  success: function(result){
+                      console.log(result);
+                      $('#videocover').attr('src',result.cover);
+                      $('#videolink').attr('href',result.video);
+                      if(result.cover === ''){
+                        $url_text_response.text('Could not retrieve video from url');
+                        $url_text_container.show().delay(5000).fadeOut();
+                      }else{
+
+                      }
+                      $('#result-container').show();
+                  },
+                  error: function(jqXHR, textStatus, error){
+                      console.log(error);
+                      $url_text_response.text('Could not retrieve video from url');
+                      $url_text_container.show().delay(5000).fadeOut();
+                  }
+              });
+        }
+      }
+      else{
+        if($('#search-bar').val() === 'http://vm.tiktok.com'){
+            $url_text_response.text('Please specify the video link');
+            $url_text_container.show().delay(5000).fadeOut();
+        }else{
+            $url_text_response.text('Please enter a valid tiktok url');
+            $url_text_container.show().delay(5000).fadeOut();
+        }
+      }
     });
 
     $('#main-loader')
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,31 +3,30 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>TicToc Converter</title>
+    <title>TIKTOK Converter</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" type="text/css" href="css/main.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-grid.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
 
 </head>
 <body>
+    <div id="cover-screen"></div>
     <!-- Logo -->
     <section class="container">
-        <div class="logo text-center">
-            <img class="d-block mx-auto" src="images/logo.png" alt="">
+        <div class="logo-container text-center">
+            <img class="d-block mx-auto" src="images/logo.svg" id="logo" alt="">
         </div>
     </section>
     <!-- Search Bar-->
     <section class="container search-bar-global-container text-center">
         <div class="title-container">
-            <h1>Online TikTok Video Downloader</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras purus nisi, euismod id tincidunt eu, feugiat id elit. Nunc imperdiet placerat fermentum. Maecenas nec egestas nisi. Ut ex diam, finibus et mi at, venenatis pulvinar sem.</p>
+            <h1 id="main-title">Online TikTok Video Downloader</h1>
+            <p id="main-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras purus nisi, euismod id tincidunt eu, feugiat id elit. Nunc imperdiet placerat fermentum. Maecenas nec egestas nisi. Ut ex diam, finibus et mi at, venenatis pulvinar sem.</p>
         </div>
         <div class="search-bar-container container text-center">
                 <div class="row search-bar-row">
@@ -41,12 +40,15 @@
         </div>
     </section>
     <!-- Results -->
-    <section class="container result-container">
+    <section class="container">
         <div class="row">
             <div class="col sm-12 spinner">
+                <div class="text-center url-text-container">
+                    <p class="text-danger" id="url-text-response">Invalid Url</p>
+                </div>
                 <div class="loader" id="main-loader"></div>
             </div>
-            <div class="col-sm-12 text-center result">
+            <div class="col-sm-12 text-center result" id="result-container">
                 <div class="overlay-image">
                     <img alt="" id="videocover" class="rounded mx-auto d-block img-thumbnail cover">
                     <div class="normal"></div>
@@ -57,16 +59,15 @@
             </div>
         </div>
     </section>
-    <footer class="footer">
-        <div class="container text-center">
-            <a href="https://scontent-lga3-1.xx.fbcdn.net/v/t66.18014-6/10000000_205975130296851_7390806429063446528_n.mp4?_nc_cat=109&efg=eyJ2ZW5jb2RlX3RhZyI6Im9lcF9oZCJ9&_nc_ht=scontent-lga3-1.xx&oh=8df706823695a9c31a3049e497625a63&oe=5C686D33" download>Download</a>
-            <span class="text-muted">TIKTOK</span>
-        </div>
+    <footer class="text-center">
+        TIKTOK DOWNLOADER 2018
     </footer>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     <!-- Jquery -->
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
     <script src="{{asset('js/main.js')}}"></script>
 </body>
 </html>
